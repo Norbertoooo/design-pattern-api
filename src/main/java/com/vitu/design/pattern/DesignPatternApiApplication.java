bb2cc197c0d1895c58357c81409f9e38a934e2e7
Vítor Norberto <vithor.norberto@gmail.com>
2022-02-26 00:53:17 -0300

    Iniciando projeto

---
+++ src/main/java/com/vitu/design/pattern/DesignPatternApiApplication.java
@@ -0,0 +1,13 @@
+package com.vitu.design.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitu/design/pattern/DesignPatternApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitu.design.pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
